37ad75d06750e52d845c940f99de2c523aead96d
Thomas Pofahl <thomas.pofahl@blendinteractive.com>
2024-04-12 10:52:03 -0500

    Add gameloop and player plugin

---
+++ src/Game/Player/Machines/IdleState.ts
@@ -0,0 +1,36 @@
+import Animated from "../../../2B2D/Components/Animated";
+import KineticBody from "../../../2B2D/Components/KineticBody";
+import MappedInput from "../../../2B2D/Components/MappedInput";
+import Sprite from "../../../2B2D/Components/Sprite";
+import Velocity from "../../../2B2D/Components/Velocity";
+import MachineState from "../../../2B2D/MachineState";
+import Update from "../../../2B2D/Update";
+import Player from "../Components/Player";
+import BasePlayerState from "./BasePlayerState";
+import WalkingState from "./WalkingState";
+
+/** Player is grounded, but not moving */
+export default class IdleState extends BasePlayerState {
+  readonly updateImmediately = true;
+
+  private constructor() { super(); }
+
+  public static readonly Instance = new IdleState();
+
+  protected onEnter(update: Update, components: { entity: number; player: Player; velocity: Velocity; animation: Animated; sprite: Sprite; body: KineticBody; }): void {
+    const { animation } = components;
+    animation.tag = 'Idle';
+  }
+
+  protected onUpdate(update: Update, components: { entity: number; player: Player; input: MappedInput, velocity: Velocity; animation: Animated; sprite: Sprite; body: KineticBody; }): MachineState | undefined {
+    const { body, player } = components;
+
+    var { left, right, up, down } = this.getKeys(update, components);
+
+   if (player.controlsEnabled && (left || right || up || down)) {
+   return WalkingState.Instance;
+   }
+
+    this.applyLeftAndRightVelocity(update, components);
+  }
+}
---
+++ src/Game/Player/Machines/WalkingState.ts
@@ -0,0 +1,35 @@
+import Animated from "../../../2B2D/Components/Animated";
+import KineticBody from "../../../2B2D/Components/KineticBody";
+import MappedInput from "../../../2B2D/Components/MappedInput";
+import Sprite from "../../../2B2D/Components/Sprite";
+import Velocity from "../../../2B2D/Components/Velocity";
+import MachineState from "../../../2B2D/MachineState";
+import Update from "../../../2B2D/Update";
+import Player from "../Components/Player";
+import BasePlayerState from "./BasePlayerState";
+import IdleState from "./IdleState";
+
+export default class WalkingState extends BasePlayerState {
+  readonly updateImmediately = true;
+
+  private constructor() { super(); }
+
+  public static readonly Instance = new WalkingState();
+
+
+  protected onEnter(update: Update, components: { entity: number; player: Player; velocity: Velocity; animation: Animated; sprite: Sprite; body: KineticBody; }): void {
+    const { animation } = components;
+    animation.tag = 'Walk';
+  }
+
+  protected onUpdate(update: Update, components: { entity: number; player: Player; input:MappedInput, velocity: Velocity; animation: Animated; sprite: Sprite; body: KineticBody; }): MachineState | undefined {
+    const { body, player } = components;
+    const { space, left, right, up, down } = this.getKeys(update, components);
+
+    if (!player.controlsEnabled || (!left && !right && !up && !down)) {
+      return IdleState.Instance;
+    }
+
+    this.applyLeftAndRightVelocity(update, components);
+  }
+}
