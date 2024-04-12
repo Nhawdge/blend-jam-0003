9accf9194fda4234e345e18f285cc19d4f79b5d2
Thomas Pofahl <thomas.pofahl@blendinteractive.com>
2024-04-12 11:43:13 -0500

    change moving wall plugin in GamePlugin.ts

--- src/Game/GamePlugin.ts
+++ src/Game/GamePlugin.ts
@@ -3,7 +3,7 @@ import LevelPlugin from "./Level/LevelPlugin";
 import InitPlugin from "./Init/InitPlugin";
 import GameLoopPlugin from "./GameLoop/GameLoopPlugin";
 import PlayerPlugin from "./Player/PlayerPlugin";
-import MovingWallPlugin from "./MovingWall/MovingWallPlugin";
+import MovingGhostPlugin from "./MovingGhost/MovingGhostPlugin";
 
 export default function GamePlugin(builder: Builder) {
   // Init plugin loads stuff, spawns camera, etc.
@@ -15,7 +15,7 @@ export default function GamePlugin(builder: Builder) {
   builder.plugin(PlayerPlugin);
 
   // moving wall plugin (obstacles that the player must avoid)
-  builder.plugin(MovingWallPlugin);
+  builder.plugin(MovingGhostPlugin);
   
   */
 
