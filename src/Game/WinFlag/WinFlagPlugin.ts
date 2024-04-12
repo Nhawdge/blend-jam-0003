5e4b6778e7871da58d9673dd3014d9c1a129d6f1
Thomas Pofahl <thomas.pofahl@blendinteractive.com>
2024-04-12 13:04:30 -0500

    changing MovingObstacle plugin

--- src/Game/WinFlag/WinFlagPlugin.ts
+++ src/Game/WinFlag/WinFlagPlugin.ts
@@ -1,5 +1,6 @@
 import Builder from "../../2B2D/Builder";
 import GameLoopState from "../GameLoop/States/GameLoopState";
+import SpawnWinFlag from "./Systems/SpawnWinFlag";
 export default function WinFlagPlugin(builder: Builder)
 {
     builder.enter(GameLoopState, SpawnWinFlag);
